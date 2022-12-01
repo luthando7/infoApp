b2be704409c55f6fd36c1f92ea516dd5eaeb73f4
Luthando Zaze <luthandozaze@gmail.com>
2022-12-01 22:33:04 +0200

    Routing for the different pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NewsPageComponent } from './news-page/news-page.component';
+import { WeatherPageComponent } from './weather-page/weather-page.component';
+import { SportsPageComponent } from './sports-page/sports-page.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent, NewsPageComponent, WeatherPageComponent, SportsPageComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent, HomePageComponent, NewsPageComponent],
 })
 export class AppModule {}
